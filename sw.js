0034a17cd3ac7c8a95e453f963e39468e80e31af
sohaibchekr <sohaibchekri@gmail.com>
2026-03-18 05:34:40 +0000

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'chronolab-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+});
